198e859868438d0b13bc588625316a284e2a2c00
phjemi <jeric.miana@e-horizon.com>
2023-11-03 16:48:32 +0800

    Initial Commit

---
+++ FacebookCloneSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FacebookCloneSwiftUI
+//
+//  Created by phjemi on 11/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacebookCloneSwiftUI/FacebookCloneSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  FacebookCloneSwiftUIApp.swift
+//  FacebookCloneSwiftUI
+//
+//  Created by phjemi on 11/3/23.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+@main
+struct FacebookCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
